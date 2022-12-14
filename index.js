8ce744cd0e5a0286e430c74159e3e21f8e253e56
aprilkrg <aprilkrgonzales@gmail.com>
2022-12-14 14:58:29 -0700

    POST to /dinosaurs

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const PORT = 3000
 const fs = require("fs")
 
 app.set("view engine", "ejs")
+app.use(express.urlencoded({extended: false}))
 
 // ROUTES
 // GET localhost:3000/dinosaurs
@@ -13,6 +14,23 @@ app.get("/dinosaurs", function(req, res) {
     console.log(dinoData)
     res.render("dinosaurs/index", {myDinos: dinoData})
 })
+
+// GET localhost:3000/dinosaurs/new
+app.get("/dinosaurs/new", function(req,res) {
+    res.render("dinosaurs/new")
+})
+
+// POST localhost:3000/dinosaurs
+app.post("/dinosaurs", function(req, res){
+    // console.log("NAME:", req.body.name
+    const dinosaurs = fs.readFileSync("./dinosaurs.json")
+    const dinoData = JSON.parse(dinosaurs)
+    dinoData.push(req.body)
+    console.log(dinoData)
+    fs.writeFileSync("./dinosaurs.json", JSON.stringify(dinoData))
+    res.redirect("/dinosaurs")
+})
+
 // GET localhost:3000/dinosaurs/:index 
 app.get("/dinosaurs/:idx", function(req,res){
     const dinosaurs = fs.readFileSync("./dinosaurs.json")
@@ -20,7 +38,6 @@ app.get("/dinosaurs/:idx", function(req,res){
     const dinoIndex = parseInt(req.params.idx)
     res.render("dinosaurs/show", {myDino: dinoData[dinoIndex]})
 })
-
 
 app.listen(PORT, function(){
     console.log("Server listening on port " + PORT)
